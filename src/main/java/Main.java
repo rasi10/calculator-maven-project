a52a10726da1af25208dd8ad43a2103ddecf4981
Rafael Silva <mind_traveller@hotmail.com>
2016-10-28 10:49:05 +0200

    Second commit. I added the test class for the class Calculator. I edited the pom.xml file of the project

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+	public static void main(String[] args){
+		Calculator calculator = new Calculator();
+		System.out.println(calculator.add(10.0, 2.0));
+		System.out.println(calculator.subtract(10.0, 2.0));
+		System.out.println(calculator.multiply(10.0, 2.0));
+		System.out.println(calculator.divide(10.0, 2.0));
+		System.out.println(calculator.divide(10.0, 0.0));
+	}
+	
+
+}
+
+
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,70 @@
+import static org.junit.Assert.*;
+
+import java.util.Random;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	Calculator calculator = new Calculator();
+	public static final Logger LOG = Logger.getLogger(Calculator.class.getName());
+	Random random = new Random();
+
+	@Test
+	public void testAdd() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double result = 0.0;
+		for (int i = 0; i < 10; i++) {
+			firstNumber = random.nextDouble()*100;
+			secondNumber = random.nextDouble()*100;
+			result = firstNumber + secondNumber;	
+			LOG.info("Testing the add method with the values: "+ firstNumber+ " and " + secondNumber);
+			assertEquals("The result should be: "+result,calculator.add(firstNumber, secondNumber), result, 0.111);
+		}
+	}
+
+	@Test
+	public void testSubtract() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double result =0.0;
+		for (int i = 0; i < 10; i++) {
+			firstNumber = random.nextDouble()*100;
+			secondNumber = random.nextDouble()*100;
+			result = firstNumber -secondNumber;
+			LOG.info("Testing the subtract method with the values" + firstNumber+" and "+ secondNumber);
+			assertEquals("The result should be: "+ result, calculator.subtract(firstNumber, secondNumber),result,0.1111);
+		}
+		
+	}
+
+	@Test
+	public void testMultiply() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double result =0.0;
+		for (int i = 0; i < 10; i++) {
+			firstNumber = random.nextDouble()*100;
+			secondNumber = random.nextDouble()*100;
+			result = firstNumber * secondNumber;
+			LOG.info("Testing the multiply method with the values" + firstNumber+" and "+ secondNumber);
+			assertEquals("The result should be: "+ result, calculator.multiply(firstNumber, secondNumber),result,0.111);
+		}
+	}
+
+	@Test
+	public void testDivide() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double result =0.0;
+		for (int i = 0; i < 10; i++) {
+			firstNumber = random.nextDouble()*100;
+			secondNumber = random.nextDouble()*100;
+			result = firstNumber / secondNumber;
+			LOG.info("Testing the divide method with the values" + firstNumber+" and "+ secondNumber);
+			assertEquals("The result should be: "+ result, calculator.divide(firstNumber, secondNumber),result,0.111);
+		}
+	}
+
+}
